0371beca83e90a39d1f1ede10f31b6f24ee1ace0
Aisyah Nabilah Putri <aisyahnabilahputri@gmail.com>
2025-11-19 14:28:01 +0700

    edit main class

--- src/GraphVisualizer.java
+++ src/GraphVisualizer.java
@@ -238,20 +238,4 @@ public class GraphVisualizer extends JFrame {
         pack();
         setLocationRelativeTo(null);
     }
-    
-    public static void main(String[] args) {
-        // Example adjacency matrix (directed weighted graph)
-        int[][] adjacencyMatrix = {
-            {0, 5, 3, 0, 0},
-            {0, 0, 2, 6, 0},
-            {0, 0, 0, 7, 4},
-            {0, 0, 0, 0, 1},
-            {0, 0, 0, 0, 0}
-        };
-        
-        SwingUtilities.invokeLater(() -> {
-            GraphVisualizer visualizer = new GraphVisualizer(adjacencyMatrix);
-            visualizer.setVisible(true);
-        });
-    }
 }
